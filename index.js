543e5753db329e8cd28ed3802198c6066f6be353
Carl Cappellino <ccappell@umd.edu>
2024-12-19 02:51:48 -0500

    Added helm charts

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors'); // Make sure this line is present
+const cors = require('cors');
+
+const { KubeConfig, CoreV1Api } = require('@kubernetes/client-node');
 
 const app = express();
 app.use(cors({ origin: '*' }));
 app.use(express.json());
+
+const kubeconfig = new KubeConfig();
+kubeconfig.loadFromDefault();
+
+const k8sApi = kubeconfig.makeApiClient(CoreV1Api);
+
+app.get('/get-secret', async (req, res) => {
+  try {
+    const secretName = 'secret-phrase';
+    const namespace = 'default';
+
+    const response = await k8sApi.readNamespacedSecret(secretName, namespace);
+    const secretData = response.body.data['secret-phrase'];
+
+    const decodedSecret = Buffer.from(secretData, 'base64').toString('utf8');
+
+    res.json({ secret: decodedSecret });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Unable to access secret!' });
+  }
+});
 
 
 mongoose.connect('mongodb://mongo-service:27017/appointments', { useNewUrlParser: true, useUnifiedTopology: true });
